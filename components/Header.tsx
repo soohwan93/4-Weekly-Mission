143f4b62482eb6ff822d505359f7aee9e203297e
soohwan93 <soohwan2im@gmail.com>
2024-04-03 04:25:03 +0900

    refactor: header를 더 세분화 하여 use client 추가해 불필요한 import까지 CSR되는 것을 제거 하기 위한 코드 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,12 @@
-"use client";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/public/logo.svg";
-import { useUserProfile } from "@/util/ContextProvider";
+import OptionalHeader from "./OptionalHeader";
+import UserProfile from "./UserProfile";
 
 function Header() {
-  const userData = useUserProfile();
-  const pathName = usePathname();
-  const isHeaderShow = pathName.includes("sign");
-  const isHeaderSticky = pathName !== "/folder";
-
-  if (isHeaderShow) return;
   return (
-    <header
-      className={`flex flex-col px-[200px] py-5 items-center self-stretch gap-2 z-10 bg-light-blue 1920px:w-full 1920px:px-calc-1 1920px:overflow-hidden 1199px:px-calc-2 869px:px-8 767px:py-[13px] ${
-        isHeaderSticky ? "sticky top-0" : "static"
-      }`}
-    >
+    <OptionalHeader>
       <div className="flex justify-between items-center self-stretch shrink-0 1920px:w-[1520px] 1199px:w-[799px] 869px:w-full ">
         <Link href="/">
           <Image
@@ -26,33 +15,9 @@ function Header() {
             className="767px:w-[88.67px] 767px:h-4"
           />
         </Link>
-        {/* <Link
-          className="flex w-32 px-5 py-3 justify-center items-center gap-2 rounded-lg font-semibold text-white text-lg no-underline shrink-0 bg-gradient-to-r from-0.12 from-[#6d6afe] to-101.84 to-[#6ae3fe] 1199px:mr-0 1199px:ml-auto 767px:w-20 767px:px-4 767px:py-[10px] 767px:text-sm"
-          href="/signin"
-        >
-          로그인
-        </Link> */}
-        {/* 로그인 페이지 접근을 쉽게 하기 위해 해당 기능 일단 주석처리했음 */}
-        {userData?.image_source ? (
-          <div className="flex gap-1">
-            <Image
-              width={24}
-              height={24}
-              src={userData.image_source}
-              alt="userProfileImg"
-            />
-            <span className="767px:hidden">{userData.email}</span>
-          </div>
-        ) : (
-          <Link
-            className="flex w-32 px-5 py-3 justify-center items-center gap-2 rounded-lg font-semibold text-white text-lg no-underline shrink-0 bg-gradient-to-r from-0.12 from-[#6d6afe] to-101.84 to-[#6ae3fe] 1199px:mr-0 1199px:ml-auto 767px:w-20 767px:px-4 767px:py-[10px] 767px:text-sm"
-            href="/signin"
-          >
-            로그인
-          </Link>
-        )}
+        <UserProfile />
       </div>
-    </header>
+    </OptionalHeader>
   );
 }
 
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,7 +2,6 @@ import { ContextProvider } from "@/util/ContextProvider";
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { useUserProfile } from "@/util/ContextProvider";
 import { ChildernProps } from "../app/layout";
 
 const Providers = ({ children }: ChildernProps) => {
