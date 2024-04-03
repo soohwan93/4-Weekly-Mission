ac2ca549b7031d2b0b23e7c4746a061d142d2fe3
soohwan93 <soohwan2im@gmail.com>
2024-04-04 01:05:40 +0900

    refactor: react-hook-form을 이용해 form 컴포넌트 개발

--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -1,50 +1,184 @@
 "use client";
-import React, { FormEvent, useRef } from "react";
-import SignInput from "./SignInput";
+import React, { useState } from "react";
 import {
   BUTTON_TEXT,
   INPUT_LABEL_TEXT,
   INPUT_SIGNUP_PLACEHOLDER_TEXT,
-  INPUT_TYPE,
+  PASSWORD_VALIDATION_TEXT,
 } from "@/util/staticValue";
-import SignForm from "./SignForm";
-import SignButton from "./SignButton";
-import { SignUrlProp } from "./SigninForm";
 
-const SignupForm = ({ pathUrl }: SignUrlProp) => {
-  const emailRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const checkPasswordRef = useRef<HTMLInputElement>(null);
+import { useRouter } from "next/navigation";
+import { FieldValues, useForm } from "react-hook-form";
+import {
+  validateSignupEmailInput,
+  validateSignupPasswordChkInput,
+  validateSignupPasswordInput,
+} from "@/util/validation";
+import { postSignupData } from "@/util/api";
+import tokenSetting from "@/util/tokenSetting";
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+const SignupForm = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    watch,
+    clearErrors,
+    setError,
+    getValues,
+  } = useForm();
+
+  const [isPasswordEyeOn, setIsPasswordEyeOn] = useState(false);
+  const [isPasswordCheckEyeOn, setIsPasswordCheckEyeOn] = useState(false);
+
+  const handlePasswordEyeClick = () => {
+    setIsPasswordEyeOn(!isPasswordEyeOn);
   };
+  const handlePasswordCheckEyeClick = () => {
+    setIsPasswordCheckEyeOn(!isPasswordCheckEyeOn);
+  };
+
+  const handleEmailBlur = async () => {
+    const emailError = await validateSignupEmailInput(watch("email"));
+    if (emailError) {
+      setError("email", {
+        type: "custom",
+        message: emailError,
+      });
+    } else {
+      clearErrors("email");
+    }
+  };
+
+  const handlePasswordBlur = () => {
+    const passwordError = validateSignupPasswordInput(watch("password"));
+    if (passwordError) {
+      setError("password", {
+        type: "custom",
+        message: passwordError,
+      });
+    } else {
+      clearErrors("password");
+    }
+  };
+
+  const handlePasswordCheckBlur = () => {
+    const passwordCheckError = validateSignupPasswordChkInput(
+      watch("password"),
+      watch("passwordCheck")
+    );
+    if (passwordCheckError) {
+      setError("passwordCheck", {
+        type: "custom",
+        message: passwordCheckError,
+      });
+    } else {
+      clearErrors("passwordCheck");
+    }
+  };
+
+  const onSubmit = async (data: FieldValues) => {
+    const result = await postSignupData(data);
+    if (result.data) {
+      tokenSetting.saveToLocalStorage(data.email, result.data.accessToken);
+      router.push("/folder");
+    }
+  };
+
   return (
-    <SignForm onSubmit={handleSubmit}>
-      <SignInput
-        path={pathUrl}
-        emailRef={emailRef}
-        labelText={INPUT_LABEL_TEXT.email}
-        placeholderText={INPUT_SIGNUP_PLACEHOLDER_TEXT.email}
-        type={INPUT_TYPE.email}
-      />
-      <SignInput
-        path={pathUrl}
-        passwordRef={passwordRef}
-        labelText={INPUT_LABEL_TEXT.password}
-        placeholderText={INPUT_SIGNUP_PLACEHOLDER_TEXT.password}
-        type={INPUT_TYPE.password}
-      />
-      <SignInput
-        passwordRef={passwordRef}
-        path={pathUrl}
-        checkPasswordRef={checkPasswordRef}
-        labelText={INPUT_LABEL_TEXT.passwordCheck}
-        placeholderText={INPUT_SIGNUP_PLACEHOLDER_TEXT.passwordCheck}
-        type={INPUT_TYPE.password}
-      />
-      <SignButton text={BUTTON_TEXT.signup} />
-    </SignForm>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col items-center gap-4"
+    >
+      <div className="relative flex flex-col items-start gap-3">
+        <label className="text-[14px] text-[#000]" htmlFor="email">
+          {INPUT_LABEL_TEXT.email}
+        </label>
+        <input
+          {...register("email", {
+            validate: (value) => validateSignupEmailInput(value),
+            onBlur: () => handleEmailBlur(),
+          })}
+          placeholder={INPUT_SIGNUP_PLACEHOLDER_TEXT.email}
+          className={`${
+            errors.email && `border-[#ff5b56] border-solid`
+          }flex w-[400px] px-[15px] py-[18px] justify-center items-center rounded-[8px] border border-[#ccd5e3] border-solid text-[#000] focus:[outline:none] focus:border-[#6d6afe] 767px:[width:min(400px,100vw-65px)]`}
+          type="email"
+        />
+        <div className={`h-4 text-xs text-[#ff5b56]`}>
+          {errors.email && (errors.email.message as string)}
+        </div>
+      </div>
+      <div className="relative flex flex-col items-start gap-3">
+        <label className="text-[14px] text-[#000]" htmlFor="password">
+          {INPUT_LABEL_TEXT.password}
+        </label>
+        <i
+          onClick={handlePasswordEyeClick}
+          data-icon="eyeIcon"
+          className={`absolute w-4 h-4 right-[15px] bg-inherit bg-no-repeat bg-[95%_90%]  ${
+            !isPasswordEyeOn
+              ? `bg-[url('../public/eye-off.svg')] top-[56px]`
+              : `bg-[url('../public/eye-on.svg')] top-[55px]`
+          } hover:cursor-pointer`}
+        />
+        <input
+          {...register("password", {
+            pattern: {
+              value: /^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/,
+              message: PASSWORD_VALIDATION_TEXT.falsy,
+            },
+            onBlur: () => handlePasswordBlur(),
+          })}
+          placeholder={INPUT_SIGNUP_PLACEHOLDER_TEXT.password}
+          className={`${
+            errors.password && `border-[#ff5b56] border-solid`
+          }flex w-[400px] px-[15px] py-[18px] justify-center items-center rounded-[8px] border border-[#ccd5e3] border-solid text-[#000] focus:[outline:none] focus:border-[#6d6afe] 767px:[width:min(400px,100vw-65px)]`}
+          type={`${isPasswordEyeOn ? `text` : `password`}`}
+        />
+        <div className={`h-4 text-xs text-[#ff5b56]`}>
+          {errors.password && (errors.password.message as string)}
+        </div>
+      </div>
+      <div className="relative flex flex-col items-start gap-3">
+        <label className="text-[14px] text-[#000]" htmlFor="password">
+          {INPUT_LABEL_TEXT.passwordCheck}
+        </label>
+        <i
+          onClick={handlePasswordCheckEyeClick}
+          data-icon="eyeIcon"
+          className={`absolute w-4 h-4 right-[15px] bg-no-repeat bg-[95%_90%]  ${
+            !isPasswordCheckEyeOn
+              ? `bg-[url('../public/eye-off.svg')] bg-inherit top-[56px]`
+              : `bg-[url('../public/eye-on.svg')] top-[55px]`
+          } hover:cursor-pointer`}
+        />
+        <input
+          {...register("passwordCheck", {
+            validate: (value) =>
+              value === getValues("password") ||
+              PASSWORD_VALIDATION_TEXT.dismatch,
+            onBlur: () => handlePasswordCheckBlur(),
+          })}
+          placeholder={INPUT_SIGNUP_PLACEHOLDER_TEXT.passwordCheck}
+          className={`${
+            errors.passwordCheck && `border-[#ff5b56] border-solid`
+          }flex w-[400px] px-[15px] py-[18px] justify-center items-center rounded-[8px] border border-[#ccd5e3] border-solid text-[#000] focus:[outline:none] focus:border-[#6d6afe] 767px:[width:min(400px,100vw-65px)]`}
+          type={`${isPasswordCheckEyeOn ? `text` : `password`}`}
+        />
+        <div className={`h-4 text-xs text-[#ff5b56]`}>
+          {errors.passwordCheck && (errors.passwordCheck.message as string)}
+        </div>
+      </div>
+      <button
+        disabled={isSubmitting}
+        type="submit"
+        className="flex w-[400px] py-4 px-5 justify-center items-center gap-[10px] [border:0px] rounded-lg bg-gradient-to-r from-[0.12%] from-[#6d6afe] to-[101.84%] to-[#6ae3fe] text-lg font-semibold text-[#fff] 767px:[width:min(400px,100vw-65px)]"
+      >
+        {BUTTON_TEXT.signup}
+      </button>
+    </form>
   );
 };
 
