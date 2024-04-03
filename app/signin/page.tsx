ecbc3cd18620994e81cdb29249780ec0456d960c
soohwan93 <soohwan2im@gmail.com>
2024-04-04 01:05:02 +0900

    refactor: 스토리지 존재 시 folder페이지 이동하도록 하는 코드 추가

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-
+"use client";
+import React, { useEffect } from "react";
 import SigninForm from "../../components/SigninForm";
 import SignHeader from "../../components/SignHeader";
 import { SIGNIN_TEXT } from "@/util/staticValue";
 import SignBanner from "../../components/SignBanner";
+import { useRouter } from "next/navigation";
 
 const Signin = () => {
+  const router = useRouter();
+  useEffect(() => {
+    if (window.localStorage.length) {
+      router.push("/folder");
+    }
+  });
+  if (window.localStorage.length) return;
   return (
     <div className="m-0 flex h-auto justify-center items-center min-h-[100vh] bg-[#f0f6ff]">
       <main className="absolute left-[50%] top-[50%] [transform:translate(-50%,-50%)] p-[30px] flex flex-col items-center gap-8">
@@ -15,7 +23,7 @@ const Signin = () => {
             linkText={SIGNIN_TEXT.headerLinkText}
             linkUrl={SIGNIN_TEXT.linkText}
           />
-          <SigninForm pathUrl={SIGNIN_TEXT.linkUrl} />
+          <SigninForm />
         </div>
         <SignBanner text={SIGNIN_TEXT.bannerText} />
       </main>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-
+"use client";
+import React, { useEffect } from "react";
 import SignHeader from "../../components/SignHeader";
 import { SIGNUP_TEXT } from "@/util/staticValue";
 import SignBanner from "../../components/SignBanner";
 import SignupForm from "../../components/SignupForm";
+import { useRouter } from "next/navigation";
 
 const Signup = () => {
+  const router = useRouter();
+  useEffect(() => {
+    if (window.localStorage.length) {
+      router.push("/folder");
+    }
+  });
+  if (window.localStorage.length) return;
   return (
     <div className="m-0 flex h-auto justify-center items-center min-h-[100vh] bg-[#f0f6ff]">
       <main className="absolute left-[50%] top-[50%] [transform:translate(-50%,-50%)] p-[30px] flex flex-col items-center gap-8">
@@ -15,7 +23,7 @@ const Signup = () => {
             linkText={SIGNUP_TEXT.headerLinkText}
             linkUrl={SIGNUP_TEXT.linkText}
           />
-          <SignupForm pathUrl={SIGNUP_TEXT.linkUrl} />
+          <SignupForm />
         </div>
         <SignBanner text={SIGNUP_TEXT.bannerText} />
       </main>
