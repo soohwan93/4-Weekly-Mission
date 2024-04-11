079fe945ce913acec4629083c3fe1ad936a8ddcf
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-12 05:12:22 +0900

    ♻️ context값을 사용하도록 코드 수정

--- components/OwnerProfile.tsx
+++ components/OwnerProfile.tsx
@@ -1,22 +1,23 @@
-import { OwnerApiData } from "@/app/shared/page";
+"use client";
+import { useUserData } from "@/util/ContextProvider";
 import React from "react";
 
-interface OwnerProfileProps {
-  name: string;
-  owner: OwnerApiData;
-}
-const OwnerProfile = ({ name, owner }: OwnerProfileProps) => {
+const OwnerProfile = ({ folderId }: { folderId: string }) => {
+  const { user, folders } = useUserData(true);
+  const folderData = folders?.filter((item) => item.id === Number(folderId))[0];
+  if (!folderData || !user) return;
+
   return (
     <section className="flex flex-col items-center gap-[30px] py-[60px] w-full bg-light-blue">
       <div className="flex flex-col items-center">
         <img
           className="w-[150px] h-[150px]"
-          src={owner.profileImageSource}
+          src={user.image_source}
           alt="ownerImage"
         />
-        <span>{owner.name}</span>
+        <span>{user.name}</span>
       </div>
-      <span className="text-[40px] font-bold">{name}</span>
+      <span className="text-[40px] font-bold">{folderData.name}</span>
     </section>
   );
 };
