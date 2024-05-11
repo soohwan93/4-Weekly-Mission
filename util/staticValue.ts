b288032eec59d436b267625baa1184c83e2dde86
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-11 17:57:55 +0900

    ♻️ 상수 속성명 대문자로 변경 및 api 관련 상수 추가

--- util/staticValue.ts
+++ util/staticValue.ts
@@ -1,58 +1,76 @@
 import { v4 as uuidv4 } from "uuid";
+const BASE_URL = "https://bootcamp-api.codeit.kr/api/linkbrary/v1";
+export const API = {
+  URL: {
+    CHECK_LOGIN_POSSIBLE: `${BASE_URL}/auth/sign-in`,
+    CHECK_EMAIL: `${BASE_URL}/users/check-email`,
+    CHECK_SIGNUP_POSSIBLE: `${BASE_URL}/auth/sign-up`,
+    USER_DATA: `${BASE_URL}/users`,
+    USER_DATA_FROM_USERID: `${BASE_URL}/users/`,
+    SHARED_FOLDER: `${BASE_URL}/folders`,
+    SHARED_FOLDER_LINK: `${BASE_URL}/folders/`,
+    FOLDER_ALL_DATA: `${BASE_URL}/links`,
+    REFRESH_TOKEN: `${BASE_URL}/auth/refresh-token`,
+  },
+  MESSAGE: {
+    COMMON: "데이터 불러오기 실패",
+    REFRESH: "다시 로그인 하세요",
+  },
+};
 
 export const INPUT_LABEL_TEXT = {
-  email: "이메일",
-  password: "비밀번호",
-  passwordCheck: "비밀번호 확인",
+  EMAIL: "이메일",
+  PASSWORD: "비밀번호",
+  PASSWORD_CHECK: "비밀번호 확인",
 };
 
 export const INPUT_SIGNIN_PLACEHOLDER_TEXT = {
-  email: "이메일을 입력해 주세요.",
-  password: "비밀번호를 입력해 주세요.",
+  EMAIL: "이메일을 입력해 주세요.",
+  PASSWORD: "비밀번호를 입력해 주세요.",
 };
 export const INPUT_SIGNUP_PLACEHOLDER_TEXT = {
-  email: "이메일을 입력해 주세요.",
-  password: "영문, 숫자를 조합해 8자 이상 입력해 주세요.",
-  passwordCheck: "비밀번호와 일치하는 값을 입력해 주세요.",
+  EMAIL: "이메일을 입력해 주세요.",
+  PASSWORD: "영문, 숫자를 조합해 8자 이상 입력해 주세요.",
+  PASSWORD_CHECK: "비밀번호와 일치하는 값을 입력해 주세요.",
 };
 
 export const BUTTON_TEXT = {
-  signin: "로그인",
-  signup: "회원가입",
+  SIGNIN: "로그인",
+  SIGNUP: "회원가입",
 };
 
 export const INPUT_TYPE = {
-  email: "email",
-  password: "password",
+  EMAIL: "email",
+  PASSWORD: "password",
 };
 
 export const SIGNIN_TEXT = {
-  headerSpanText: "회원이 아니신가요?",
-  headerLinkText: "회원 가입하기",
-  bannerText: "소셜 로그인",
-  linkText: "/signup",
-  linkUrl: "/signin",
+  HEADER_SPAN_TEXT: "회원이 아니신가요?",
+  HEADER_LINK_TEXT: "회원 가입하기",
+  BANNER_TEXT: "소셜 로그인",
+  LINK_TEXT: "/signup",
+  LINK_URL: "/signin",
 };
 
 export const SIGNUP_TEXT = {
-  headerSpanText: "이미 회원이신가요?",
-  headerLinkText: "로그인 하기",
-  bannerText: "다른 방식으로 가입하기",
-  linkText: "/signin",
-  linkUrl: "/signup",
+  HEADER_SPAN_TEXT: "이미 회원이신가요?",
+  HEADER_LINK_TEXT: "로그인 하기",
+  BANNER_TEXT: "다른 방식으로 가입하기",
+  LINK_TEXT: "/signin",
+  LINK_URL: "/signup",
 };
 
 export const EMAIL_VALIDATION_TEXT = {
-  empty: "이메일을 입력해 주세요.",
-  falsy: "올바른 이메일 주소가 아닙니다.",
-  fail: "이메일을 확인해 주세요.",
+  EMPTY: "이메일을 입력해 주세요.",
+  FALSY: "올바른 이메일 주소가 아닙니다.",
+  FAIL: "이메일을 확인해 주세요.",
 };
 
 export const PASSWORD_VALIDATION_TEXT = {
-  empty: "비밀번호를 입력해 주세요.",
-  falsy: "비밀번호는 영문,숫자 조합 8자 이상 입력해 주세요.",
-  dismatch: "비밀번호가 일치하지 않아요.",
-  fail: "비밀번호를 확인해 주세요.",
+  EMPTY: "비밀번호를 입력해 주세요.",
+  FALSY: "비밀번호는 영문,숫자 조합 8자 이상 입력해 주세요.",
+  DISMATCH: "비밀번호가 일치하지 않아요.",
+  FAIL: "비밀번호를 확인해 주세요.",
 };
 
 export const USER_INITIAL_VALUE = {
@@ -135,12 +153,22 @@ export const ADD_MODAL_ITEMS = [
 ];
 
 export const MODAL_TYPE = {
-  share: "share",
-  editFolder: "editFolder",
-  deleteFolder: "deleteFolder",
-  deleteLink: "deleteLink",
-  addFolder: "addFolder",
-  add: "add",
+  SHARE: "share",
+  EDIT_FOLDER: "editFolder",
+  DELETE_FOLDER: "deleteFolder",
+  DELETE_LINK: "deleteLink",
+  ADD_FOLDER: "addFolder",
+  ADD: "add",
 };
 
 export const ALL_LIST_BUTTON_ID = 0;
+
+export const QUERY_KEY = {
+  SHARED_FOLDER_LIST: "sharedFolderList",
+  SHARED_FOLDER_LIST_BY_FOLDER_ID: "sharedFolderListByFolderId",
+  USER_PROFILE: "userProfile",
+  USER_PROFILE_BY_ID: "userProfileById",
+  SHARED_LINK_LIST: "sharedLinkList",
+  FOLDER_ALL_LINK_LIST: "folderAllLinkList",
+  FOLDER_LINK_LIST: "folderLinkList",
+};
