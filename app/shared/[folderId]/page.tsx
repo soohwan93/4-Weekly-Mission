c8fc6520919e57735aa7ffd285439d5ef3add002
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-11 17:46:08 +0900

    ♻️ prop 추가

--- app/shared/[folderId]/page.tsx
+++ app/shared/[folderId]/page.tsx
@@ -8,7 +8,7 @@ const Shared = ({ params }: { params: { folderId: string } }) => {
       <div className="flex flex-col items-center">
         <OwnerProfile folderId={params.folderId} />
         <section className="flex flex-col items-center gap-10 py-10 max-w-[1060px] min-h-[1200px] w-full 1124px:py-10 1124px:px-8">
-          <SharedMain />
+          <SharedMain folderId={params.folderId} />
         </section>
       </div>
     </>
