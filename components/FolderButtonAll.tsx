5158539d6caf110fc3cabcd841bebfd6518711c5
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-11 17:52:22 +0900

    ♻️ 변경된 provider명으로 인한 코드 수정

--- components/FolderButtonAll.tsx
+++ components/FolderButtonAll.tsx
@@ -2,7 +2,7 @@ import AddImage from "./AddImage";
 import Link from "next/link";
 import { MODAL_TYPE } from "@/util/staticValue";
 import { OnModalProps } from "../app/folder/page";
-import { Folders } from "@/util/ContextProvider";
+import type { Folders } from "@/util/contexts/UserDataProvider";
 import { useRouter } from "next/navigation";
 
 export type handleButtonListItemClick = (id?: number) => void;
@@ -35,7 +35,7 @@ function FolderButtonAll({ folderList, onModal }: FolderButtonProps) {
       <Link
         href={"#"}
         className="flex gap-1 whitespace-nowrap items-center justify-center text-[#6d6afe] text-center font-medium tracking-[-0.3px] 767px:fixed 767px:bottom-[101px] 767px:left-[50%] 767px:[transform:translate(-50%,-50%)] 767px:z-[10] 767px:rounded-[20px] 767px:border-[1px] 767px:border-solid 767px:border-[#6d6afe] 767px:bg-[#6d6afe] 767px:text-[#e7effb] 767px:flex 767px:py-2 767px:px-6 767px:items-start 767px:gap-[10px]"
-        onClick={() => onModal(MODAL_TYPE.addFolder)}
+        onClick={() => onModal(MODAL_TYPE.ADD_FOLDER)}
       >
         <span>폴더 추가</span>
         <AddImage className="text-[#6d6afe] 767px:text-[#fff]" />
