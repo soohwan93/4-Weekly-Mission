40c995713008d2c27c4600be62371281efe5519e
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-11 17:45:34 +0900

    ♻️ 불필요한 코드 제거(middleware로 제어)

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,32 +1,22 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 import SigninForm from "../../components/SigninForm";
 import SignHeader from "../../components/SignHeader";
 import { SIGNIN_TEXT } from "@/util/staticValue";
 import SignBanner from "../../components/SignBanner";
-import { redirect } from "next/navigation";
-import { useUserData } from "@/util/ContextProvider";
 
 const Signin = () => {
-  const { user } = useUserData();
-
-  useEffect(() => {
-    if (user) {
-      redirect("/folder");
-    }
-  }, [user, redirect]);
   return (
     <div className="m-0 flex h-auto justify-center items-center min-h-[100vh] bg-[#f0f6ff]">
       <main className="absolute left-[50%] top-[50%] [transform:translate(-50%,-50%)] p-[30px] flex flex-col items-center gap-8">
         <div className="flex flex-col items-center gap-[30px]">
           <SignHeader
-            spanText={SIGNIN_TEXT.headerSpanText}
-            linkText={SIGNIN_TEXT.headerLinkText}
-            linkUrl={SIGNIN_TEXT.linkText}
+            spanText={SIGNIN_TEXT.HEADER_SPAN_TEXT}
+            linkText={SIGNIN_TEXT.HEADER_LINK_TEXT}
+            linkUrl={SIGNIN_TEXT.LINK_TEXT}
           />
           <SigninForm />
         </div>
-        <SignBanner text={SIGNIN_TEXT.bannerText} />
+        <SignBanner text={SIGNIN_TEXT.BANNER_TEXT} />
       </main>
     </div>
   );
