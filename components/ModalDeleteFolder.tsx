60e0a9284fb167fcf242f027fa7d14f9772ce44a
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-18 22:37:26 +0900

    ♻️ 폴더 삭제시 폴더명이 명시되도록 수정

--- components/ModalDeleteFolder.tsx
+++ components/ModalDeleteFolder.tsx
@@ -1,7 +1,10 @@
 function ModalDeleteFolder({ folderName }: { folderName: string }) {
   return (
     <>
-      <div className="text-[20px] font-semibold">폴더 삭제</div>
+      <div className="flex flex-col items-center gap-2">
+        <div className="text-[20px] font-semibold">폴더 삭제</div>
+        <span className="text-[14px] text-[#9fa6b2]">{folderName}</span>
+      </div>
       <button className="[border:none] w-full h-[50px] text-white font-semibold rounded-[10px] bg-[#ff5b56]">
         삭제하기
       </button>
