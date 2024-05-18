f3f62b6ea0486c1325dd7a1218625b26d787e604
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-18 22:45:54 +0900

    🔥 불필요한 파일 제거

--- util/ModalPortal.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-import ReactDom from "react-dom";
-
-const ModalPortal = ({ children }: { children: ReactNode }) => {
-  const el = document.getElementById("modal-root") as Element;
-  return ReactDom.createPortal(children, el);
-};
-
-export default ModalPortal;
--- util/metadata.ts
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  title: "Linkbrary",
-  description: "세상의 모든 정보를 쉽게 저장하고 관리해 보세요",
-  image:
-    "https://i0.wp.com/library.re.kr/wp-content/uploads/2022/05/996907.jpg?resize=1080%2C675&ssl=1",
-  url: "https://weekly-mission-week2-ksh.netlify.app/",
-};
