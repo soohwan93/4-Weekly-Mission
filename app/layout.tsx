2305cdd1506bf4505d04aea583e50926b608f19a
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-18 22:25:07 +0900

    ♻️ metadata리팩토링

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,43 @@
 import "./globals.css";
-import Head from "next/head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { UserDataProvider } from "@/util/contexts/UserDataProvider";
-import { metadata } from "@/util/metadata";
+import { UserProvider } from "@/util/contexts/UserProvider";
+
 import ReactQueryProviders from "@/util/contexts/ReactQueryProvider";
 
+export const metadata = {
+  title: "Linkbrary",
+  description: "세상의 모든 정보를 쉽게 저장하고 관리해 보세요",
+  openGraph: {
+    type: "website",
+    url: "https://weekly-mission-week2-ksh.netlify.app/",
+    title: "Linkbrary",
+    description: "세상의 모든 정보를 쉽게 저장하고 관리해 보세요",
+    images: [
+      {
+        url: "https://i0.wp.com/library.re.kr/wp-content/uploads/2022/05/996907.jpg?resize=1080%2C675&ssl=1",
+        width: 1080,
+        height: 675,
+        alt: "Linkbrary Thumbnail",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    url: "https://weekly-mission-week2-ksh.netlify.app/",
+    title: "Linkbrary",
+    description: "세상의 모든 정보를 쉽게 저장하고 관리해 보세요",
+    images: [
+      {
+        url: "https://i0.wp.com/library.re.kr/wp-content/uploads/2022/05/996907.jpg?resize=1080%2C675&ssl=1",
+        alt: "Linkbrary Thumbnail",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 export interface ChildernProps {
   children: React.ReactNode;
 }
@@ -13,34 +45,13 @@ export interface ChildernProps {
 export default function RootLayout({ children }: ChildernProps) {
   return (
     <html lang="ko">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
-        />
-        {/* Open Graph */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={metadata.url} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        {/* Twitter Card */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content={metadata.url} />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.image} />
-      </Head>
       <body className="font-Pretendard">
         <ReactQueryProviders>
-          <UserDataProvider>
+          <UserProvider>
             <Header />
             {children}
             <Footer />
-          </UserDataProvider>
+          </UserProvider>
           <div id="modal-root" />
         </ReactQueryProviders>
       </body>
