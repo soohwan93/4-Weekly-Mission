65962d09d6b8c2d1fce2cafb5abffd96dd941923
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-18 22:45:34 +0900

    ♻️ api의 확장성을 고려해 전면 수정, 불필요한 refreshToken관련 코드 제거, 한눈에 봤을 때 더 endpoint가 바로 보여 명시적일 수 있도록 지나친 상수화 제거

--- util/api.ts
+++ util/api.ts
@@ -1,14 +1,14 @@
 "use client";
 import { FieldValues } from "react-hook-form";
-import {
-  getCookie,
-  setAccessCookieToken,
-  setRefreshCookieToken,
-} from "./cookieSetting";
-import { API } from "./staticValue";
+import { getCookie } from "./cookieSetting";
+import { BASE_URL } from "./staticValue";
+interface Favorite {
+  favorite: boolean;
+}
 interface EmailObject {
   email: string;
 }
+type Method = "GET" | "POST" | "PUT" | "DELETE";
 
 async function fetchWithToken(url: string, options: RequestInit = {}) {
   const headers = new Headers(options.headers as HeadersInit);
@@ -29,96 +29,64 @@ async function fetchWithToken(url: string, options: RequestInit = {}) {
 
   let response = await fetch(url, mergedOptions);
 
-  if (response.status === 401) {
-    const refreshTokenResponse = await fetch(API.URL.REFRESH_TOKEN, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        refresh_token: getCookie("refreshToken"),
-      }),
-    });
-    if (refreshTokenResponse.ok) {
-      const refreshTokenData = await refreshTokenResponse.json();
-      const accessToken = refreshTokenData.data.accessToken;
-      const refreshToken = refreshTokenData.data.refreshToken;
-
-      setAccessCookieToken(accessToken);
-      setRefreshCookieToken(refreshToken);
-      headers.set("Authorization", `Bearer ${accessToken}`);
-
-      const updatedOptions: RequestInit = {
-        ...mergedOptions,
-        headers: headers,
-      };
-
-      response = await fetch(url, updatedOptions);
-    } else {
-      return new Error(API.MESSAGE.REFRESH);
-    }
-  }
-
   if (!response.ok) {
-    return new Error(API.MESSAGE.COMMON);
+    const errorResponse = await response.json();
+    alert(errorResponse.message);
+    console.error(errorResponse.message);
+    return;
   }
 
   return response.json();
 }
 
-async function getApiResponse(url: string) {
-  const body = await fetchWithToken(url);
-  return body;
-}
+async function fetcher(endpoint: string, method: Method, body?: any) {
+  const options: RequestInit = {
+    method,
+    body: body ? JSON.stringify(body) : undefined,
+  };
 
-async function postApiResponse(url: string, bodyData: Object) {
-  const body = await fetchWithToken(url, {
-    method: "POST",
-    body: JSON.stringify(bodyData),
-  });
-  return body;
+  return await fetchWithToken(`${BASE_URL}${endpoint}`, options);
 }
 
 export function getSharedUser() {
-  return getApiResponse(API.URL.USER_DATA);
+  return fetcher(`/users`, "GET");
 }
 
 export function getSharedUserById(userId: number) {
-  const requestUrl = API.URL.USER_DATA_FROM_USERID + userId;
-  return getApiResponse(requestUrl);
+  return fetcher(`/users/${userId}`, "GET");
 }
 
 export function getFolderList() {
-  return getApiResponse(API.URL.SHARED_FOLDER);
+  return fetcher(`/folders`, "GET");
 }
 export function getFolderListByFolderId(folderId: number) {
-  const requestUrl = API.URL.SHARED_FOLDER_LINK + folderId;
-  return getApiResponse(requestUrl);
+  return fetcher(`/folders/${folderId}`, "GET");
 }
 
 export function getFolderListLink(folderId: number) {
-  const requestUrl = `${API.URL.SHARED_FOLDER_LINK}${folderId}/links`;
-  return getApiResponse(requestUrl);
+  return fetcher(`/folders/${folderId}/links`, "GET");
 }
 
-export async function getFolderListData(folderId?: number) {
-  const requestUrl = folderId
-    ? `${API.URL.SHARED_FOLDER_LINK}${folderId}/links`
-    : API.URL.FOLDER_ALL_DATA;
-  return getApiResponse(requestUrl);
+export async function getFolderData() {
+  return fetcher(`/links`, "GET");
+}
+
+export async function getFolderListData(folderId: number) {
+  return fetcher(`/folders/${folderId}/links`, "GET");
 }
 
 export function postLoginData(loginData: FieldValues) {
-  const requestUrl = API.URL.CHECK_LOGIN_POSSIBLE;
-  return postApiResponse(requestUrl, loginData);
+  return fetcher(`/auth/sign-in`, "POST", loginData);
 }
 
 export function postSignupEmailValidationData(email: EmailObject) {
-  const requestUrl = API.URL.CHECK_EMAIL;
-  return postApiResponse(requestUrl, email);
+  return fetcher(`/users/check-email`, "POST", email);
 }
 
 export function postSignupData(loginData: FieldValues) {
-  const requestUrl = API.URL.CHECK_SIGNUP_POSSIBLE;
-  return postApiResponse(requestUrl, loginData);
+  return fetcher(`/auth/sign-up`, "POST", loginData);
+}
+
+export function putChangeFavorite(linkId: number, bodyData: Favorite) {
+  return fetcher(`/links/${linkId}`, "PUT", bodyData);
 }
