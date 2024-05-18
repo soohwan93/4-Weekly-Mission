6d2058e34ff4805b02f7f9e17bb650dc39d35731
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-18 22:31:49 +0900

    ♻️ 컴포넌트명 변경

--- components/FolderLinkContainer.tsx
+++ components/FolderLinkContainer.tsx
@@ -1,46 +1,49 @@
+"use client";
 import noImage from "@/public/no-image.png";
-import { OnModalProps } from "../app/folder/page";
+import { FolderListApiItem, OnModalProps } from "../app/folder/page";
 import starOffImg from "@/public/star-off.png";
 import starOnImg from "@/public/star-on.png";
 
 import { MouseEvent, useState } from "react";
-import { FolderListApiItem } from "./FolderListItem";
 import { formatDate, formatDateRelative } from "@/util/formatDate";
 import Kebab from "./Kebab";
+import { putChangeFavorite } from "@/util/api";
 
 export interface ListItemProps extends OnModalProps {
   item: FolderListApiItem;
 }
 
-function FolderLinkConatiner({ item, onModal }: ListItemProps) {
-  const [starred, setStarred] = useState(false);
-  const handleStarClick = (e: MouseEvent) => {
+function FolderLinkContainer({ item, onModal }: ListItemProps) {
+  const [starred, setStarred] = useState(!!item.favorite);
+
+  const handleStarClick = async (e: MouseEvent) => {
     e.preventDefault();
+    const body = { favorite: !starred };
+    await putChangeFavorite(item.id, body);
     setStarred(!starred);
   };
+
   return (
     <a
-      className="w-full h-full relative rounded-[20px] shadow-lg flex flex-col gap-[15px] no-underline text-inherit hover:bg-[#f0f6ff]"
+      className="w-full h-full relative rounded-[20px] shadow-lg flex flex-col gap-[15px] no-underline text-inherit bg-white hover:bg-[#f0f6ff]"
       href={item.url}
       target="_blank"
       rel="noopener noreferrer"
-      key={item.id}
     >
-      <div className="overflow-hidden h-[170px] relative [border-top-left-radius:20px] [border-top-right-radius:20px]">
+      <div className=" overflow-hidden h-[170px] relative [border-top-left-radius:20px] [border-top-right-radius:20px]">
         <img
           className="w-full h-[170px] object-cover [transition-property:transform] [transition-duration:0.5s] [transition-timing-function:ease] hover:scale-[1.3]"
           src={item.image_source || noImage.src}
           alt={String(item.id) || "no-Image"}
         />
-        {starred && (
+        {starred ? (
           <img
             className="absolute right-[15px] top-[15px]"
             onClick={(e) => handleStarClick(e)}
             src={starOnImg.src}
             alt="starOnImg"
           />
-        )}
-        {!starred && (
+        ) : (
           <img
             className="absolute right-[15px] top-[15px]"
             onClick={(e) => handleStarClick(e)}
@@ -63,4 +66,4 @@ function FolderLinkConatiner({ item, onModal }: ListItemProps) {
   );
 }
 
-export default FolderLinkConatiner;
+export default FolderLinkContainer;
