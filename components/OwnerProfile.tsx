04a3b242db5633eeb74ba40094da96f74baf5c23
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-05-18 22:37:50 +0900

    ♻️ 상위컴포넌트로 위임

--- components/OwnerProfile.tsx
+++ components/OwnerProfile.tsx
@@ -1,34 +1,16 @@
-"use client";
-import { getSharedUserById } from "@/util/api";
-import { Folders } from "@/util/contexts/UserDataProvider";
-import { useFolderByFolderIdQuery } from "@/util/hooks/useFolderQueryByFolderId";
-import { useUserQuery } from "@/util/hooks/useUserQuery";
-import { QUERY_KEY } from "@/util/staticValue";
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import OwnerProfileSkeleton from "./OwnerProfileSkeleton";
-
-const OwnerProfile = ({ folderId }: { folderId: string }) => {
-  const { data: foldersByFolderId } = useFolderByFolderIdQuery(
-    Number(folderId)
-  );
-  const { data: userData } = useUserQuery();
-  const { data: userDataById, isPending } = useQuery({
-    queryKey: [QUERY_KEY.USER_PROFILE_BY_ID],
-    queryFn: () => getSharedUserById(user.id),
-    enabled: !!userData,
-    staleTime: 0,
-  });
-  const user = userData?.[0] ?? [];
-
-  const userById = userDataById?.[0] ?? [];
-
-  const folderData = foldersByFolderId?.filter(
-    (item: Folders) => item.id === Number(folderId)
-  )[0];
-
+interface OwnerProfileProps {
+  folderData: any;
+  userById: any;
+  isPending: any;
+}
+const OwnerProfile = ({
+  folderData,
+  userById,
+  isPending,
+}: OwnerProfileProps) => {
   if (!folderData || !userById || isPending) return <OwnerProfileSkeleton />;
-
   return (
     <section className="flex flex-col h-[396px] items-center gap-[30px] py-[60px] w-full bg-light-blue">
       <div className="flex flex-col items-center gap-3">
